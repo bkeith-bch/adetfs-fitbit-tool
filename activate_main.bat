1ffb28f8a3829c533320ea51d69f321e804ab1aa
herissonbleu <herisson_bleu@outlook.com>
2022-08-06 11:48:40 +0200

    Added some date strings to be used if output is saved in files

--- activate_main.bat
+++ activate_main.bat
@@ -2,6 +2,14 @@ REM @echo off
 powershell -window minimized -command ""
 SET /A counter = 0
 
+for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
+     set dow=%%i
+     set month=%%j
+     set day=%%k
+     set year=%%l
+)
+set datestr=%month%_%day%_%year%
+
 :ping
 ping -n 1 www.google.com -w 1000 > null && goto a
 REM No internet access or access too slow. Sleep for 10 minutes
