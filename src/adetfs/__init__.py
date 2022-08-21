efedf7b2c09d5ab3b029e2a2735a8bfa008c1fb8
herissonbleu <herisson_bleu@outlook.com>
2022-08-21 17:14:42 +0200

    Updating version

--- src/adetfs/__init__.py
+++ src/adetfs/__init__.py
@@ -5,7 +5,7 @@ except ModuleNotFoundError:
     import toml as tomllib
 
 # Version of the adetfs package
-__version__ = "1.1.rc0"
+__version__ = "1.1.0rc0"
 
 _cfg = tomllib.loads(resources.read_text("adetfs", "config.toml"))
 URL = _cfg["homepage"]["url"]
